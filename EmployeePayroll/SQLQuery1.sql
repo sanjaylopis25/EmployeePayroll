d75ca440da6e160b0a5009a5bc83324ea9e0d60a
sanajylopis25 <sanajylopis25@gmail.com>
2022-01-03 06:27:45 +0530

    UC2Query

--- EmployeePayroll/SQLQuery1.sql
+++ EmployeePayroll/SQLQuery1.sql
@@ -2,5 +2,20 @@
 
 use payroll_service1
 
+-- UC2
+create table employee_payroll(
+Id int primary key identity,
+name varchar(50),
+phone varchar(15),
+address varchar(100),
+department varchar(20),
+gender varchar(1),
+basic_pay money,
+deduction money,
+taxable_pay money,
+tax money,
+netpay money,
+startDate date
+);
 
 
